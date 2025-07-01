c186c656a8431a9d9554451064783d661b9bd942
blueswayne133 <blueswayne133@gmail.com>
2025-07-01 05:36:16 -0700

    update

--- app/Http/Controllers/User/DepositController.php
+++ app/Http/Controllers/User/DepositController.php
@@ -50,7 +50,7 @@ class DepositController extends Controller
 
         // Validate the request
         $validatedData = $request->validate([
-            'amount' => 'required|numeric|min:10', // Minimum deposit of 10
+            'amount' => 'required|numeric|min:1000', // Minimum deposit of 10
             'account' => 'required|string|in:holding,trading,mining,staking' // Match your select options
         ]);
 
